96e18cfe72492347757c8e9a6113f43bfde9a24b
Gadiel Russell <gadiel.russell@gmail.com>
2020-12-29 22:06:50 -0600

    Initial commit.

---
+++ AmazonReviewGenerator/AmazonReviewGenerator.API/Controllers/GenerateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonReviewGenerator.API.Controllers
+{
+    [Route("[controller]")]
+    public class GenerateController : Controller
+    {
+        public GenerateController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GenerateReview()
+        {
+
+
+
+            return Ok();
+        }
+    }
+}
